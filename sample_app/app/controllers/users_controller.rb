30448633f1a54a2c65151c62cb03a8064cfaec09
longtime1116 <longtime1116@gmail.com>
2017-06-18 02:09:46 +0900

    rails_10.3.1

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
@@ -18,6 +19,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
