d84ecf2aa26874d6342a65ef7b6bbc0c80f5224b
longtime1116 <longtime1116@gmail.com>
2017-06-24 02:40:50 +0900

    rails_13_3_3

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -47,15 +47,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  # ログイン済みユーザかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   # 正しいユーザかどうか確認
   def correct_user
     @user = User.find(params[:id])
